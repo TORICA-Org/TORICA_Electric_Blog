5be988289b5abe07e32bd20f2ba1d0d436d53f21
00kenno <114tukaiyasui@gmail.com>
2025-09-12 02:10:47 +0900

    miss

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,1 @@
-// 1. 正しい関数名に修正
 alert('アラートのメッセージ');
-
-// 2. 要素の存在を確認してから実行
-//const viewportMeta = document.querySelector("meta[name='viewport']");
-
-//if (viewportMeta) {
-  //viewportMeta.setAttribute("content", "width=device-width,user-scalable=no,shrink-to-fit=yes");
-//}
