79ccef8af1e56eb7e0f73fcef849b1715ecee31f
00kenno <162081854+00kenno@users.noreply.github.com>
2025-09-11 19:21:22 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,2 @@
 alert('アラートのメッセージ');
-document.querySelector("meta[name='viewport']").setAttribute("content", "width=device-width,user-scalable=no,shrink-to-fit=yes">)
+document.querySelector("meta[name='viewport']").setAttribute("content", "width=device-width,user-scalable=no,shrink-to-fit=yes">);
